5b42ea6ee7964b91842e1b2ac03a2ab0ca78790e
Cooper <cmphill@pm.me>
2023-07-04 15:44:23 -0600

    got the tests passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+
+    function focuser(){
+        console.log('Good!')
+    }
+    function blurrer(){
+        console.log('Hey! Eyes on me!')
+    }
+    
+return(
+    <div>
+        <button
+        onFocus={focuser}
+        onBlur={blurrer}
+        >Eyes on me</button>
+    </div>
+)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange() {
+        console.log("Entering password...");
+    }
+    
     return (
-        <div></div>
+        
+        <div>
+            <input 
+            onChange={handleChange}
+            type="password"
+             />
+
+        </div>
     )
 }
 
